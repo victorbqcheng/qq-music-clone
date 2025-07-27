6bd0af97a1a44c9f9129c07e568ef4be6bfc0599
victor <victoratnz@outlook.com>
2025-07-27 20:52:54 +0800

    feat: add Controlbar, Interactions, and Utils components to Footer

---
+++ src/Page2/Footer/Controlbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Slider } from "antd";
+import { TfiLoop } from "react-icons/tfi";
+import { AiOutlineStepForward, AiOutlineStepBackward } from "react-icons/ai";
+import { IoPlaySharp } from "react-icons/io5";
+import { RxSpeakerModerate } from "react-icons/rx";
+
+
+const Controlbar = () => {
+    return (
+        <div className='flex flex-col items-center justify-center w-1/3 pointer-events-none'>
+            <div className='flex flex-row items-center justify-around w-full'>
+                <div className='cursor-pointer hover:text-green-400 pointer-events-auto'><TfiLoop /></div>
+                <div className='cursor-pointer hover:text-green-400 pointer-events-auto'><AiOutlineStepBackward /> </div>
+                <div className='cursor-pointer bg-green-400 p-2 rounded-2xl w-10 flex flex-row items-center justify-center pointer-events-auto'><IoPlaySharp /></div>
+                <div className='cursor-pointer hover:text-green-400 pointer-events-auto'><AiOutlineStepForward /></div>
+                <div className='cursor-pointer hover:text-green-400 pointer-events-auto'><RxSpeakerModerate /></div>
+            </div>
+            <div className='flex flex-row items-center justify-between w-full gap-2'>
+                <div className='text-sm text-gray-600 pointer-events-auto'>00:00</div>
+                <div className='flex-1 pointer-events-auto'>
+                    <Slider defaultValue={30} className='w-full'  />
+                </div>
+                <div className='text-sm text-gray-600 ml-2 pointer-events-auto'>04:20</div>
+            </div>
+        </div>
+    )
+}
+
+export default Controlbar
--- src/Page2/Footer/Footer.tsx
+++ src/Page2/Footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BsChevronContract } from 'react-icons/bs'
+
+import stateStore from '../../store/stateStore'
+import Interactions from './Interactions'
+import Controlbar from './Controlbar'
+import Utils from './Utils'
 
 const Footer = () => {
     return (
-        <div className='h-20 w-full bg-blue-500 text-white flex items-center justify-center'>
-            Footer
+        <div className='absolute bottom-0 h-20 w-full flex items-center justify-center'>
+            <BsChevronContract className='text-2xl hover:text-green-300 ml-4' onClick={()=>stateStore.setShowPage2(false)} />
+            <div className='flex-1 flex items-center justify-between gap-4 h-full bg-amber-200'>
+                <Interactions />
+                <Controlbar />
+                <Utils />
+            </div>
         </div>
     )
 }
