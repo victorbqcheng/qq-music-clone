3b44a5273f704e3235888f5be2f9edb1f401852d
victor <victoratnz@outlook.com>
2025-07-28 17:19:10 +0800

    feat: add AudioFileInfo interface to define audio file metadata structure

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+
+export declare interface AudioFileInfo {
+    title: string;
+    artist: string;
+    album: string;
+    duration: number;
+    filePath: string;
+    img: string | null;
+    fileSize: number;
+};
+
