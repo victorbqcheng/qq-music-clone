40f9044f0b1e992968530d77b6475ed3a01a58d2
victor <3074146158@qq.com>
2025-07-30 20:16:12 +0800

    feat: refactor Main component to improve structure and integrate player context for animation control

--- src/Page2/Main/Main.tsx
+++ src/Page2/Main/Main.tsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import LyricsDisplay from './LyricsDisplay';
 import album1 from '../../assets/album1.jpg';
 import Cover from './Cover';
+import { usePlayer } from '../../context/PlayerContext';
 
-const Main = () => {
-    const [currentTime, setCurrentTime] = useState(0);
-
-    // 示例歌词数据
+// 示例歌词数据
     const lyrics = [
         { time: 0.0, text: "这是第一句歌词" },
-        { time: 5.3, text: "这是第二句歌词" },
-        { time: 10.2, text: "这是第三句歌词" },
+        { time: 5.0, text: "这是第二句歌词" },
+        { time: 10.0, text: "这是第三句歌词" },
         { time: 15.0, text: "这是第四句歌词" },
         { time: 20.0, text: "这是第五句歌词" },
         { time: 25.0, text: "这是第六句歌词" },
@@ -28,22 +26,26 @@ const Main = () => {
         
         // 更多歌词...
     ];
+
+const Main = () => {
+    const [currentTime, setCurrentTime] = useState(0);
+    const rotateDivRef = useRef<HTMLDivElement>(null);
+    const {state:{isPlaying}} = usePlayer();
+    useEffect(()=>{
+        if(rotateDivRef.current){
+            rotateDivRef.current.style.animationPlayState = isPlaying ? 'running' : 'paused';
+        }
+    }, [isPlaying]);
+    
     return (
         <div className='flex-1 w-full overflow-y-auto flex flex-row bg-[#808080]'>
-            <div className='flex-1 overflow-y-auto flex items-center justify-center'>
-                <div className='w-96 h-96'>
+            <div className='flex-1 flex items-center justify-center'>
+                <div ref={rotateDivRef} className='w-96 h-96' style={{animation: 'rotate 10s linear infinite'}}>
                     <Cover imageUrl={album1} />
                 </div>
             </div>
             <div className='flex-1 flex flex-col items-center justify-center'>
                 <div className='flex flex-col h-2/3 w-4/5  overflow-y-auto'>
-                    {/* {
-                        Array.from({ length: 12 }, (_, i) => (
-                            <div key={i} className='p-4 border-b border-gray-200'>
-                                Item {i + 1}
-                            </div>
-                        ))
-                    } */}
                     <LyricsDisplay lyrics={lyrics} currentTime={currentTime} />
                     <button onClick={() => setCurrentTime(currentTime + 5)}>向下滚动</button>
                 </div>
