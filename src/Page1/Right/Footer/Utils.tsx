af6d0a54ff12e30f3d59de45c186cbb5500ea4e0
victor <victoratnz@outlook.com>
2025-08-01 00:10:53 +0800

    feat: add state management for opening the current playlist in Utils component

--- src/Page1/Right/Footer/Utils.tsx
+++ src/Page1/Right/Footer/Utils.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BsSoundwave } from 'react-icons/bs'
 import { MdPlaylistPlay } from "react-icons/md";
+import stateStore from '../../../store/stateStore';
 
 const Utils = () => {
     return (
@@ -8,7 +9,8 @@ const Utils = () => {
             <div className='hover:text-green-400 text-xs p-1 rounded-sm border-1 pointer-events-auto'>标准</div>
             <BsSoundwave className='w-6 h-6 hover:text-green-400 pointer-events-auto'/>
             <div className='hover:text-green-400 text-xs p-1 rounded-sm border-1 pointer-events-auto'>词</div>
-            <MdPlaylistPlay className='w-6 h-6 hover:text-green-400 pointer-events-auto' />
+            <MdPlaylistPlay className='w-6 h-6 hover:text-green-400 pointer-events-auto'
+                onClick={()=>stateStore.setOpenCurrentPlayList(!stateStore.openCurrentPlayList)} />
         </div>
     )
 }
--- src/store/stateStore.tsx
+++ src/store/stateStore.tsx
@@ -4,6 +4,7 @@ class StateStore{
     
     showPage2 = false;
     mountPage2 = false; // Whether to mount the Page2 component. Prevents Page2 from fading out on app startup: On startup, Page2 should not use fadeOut; after Page2 appears for the first time, use fadeOut for subsequent hides.
+    openCurrentPlayList = false; // Whether to open the current playlist
     constructor() {
         makeAutoObservable(this);
     }
@@ -13,6 +14,10 @@ class StateStore{
 
     setMountPage2(value: boolean) {
         this.mountPage2 = value;
+    }
+    
+    setOpenCurrentPlayList(value: boolean) {
+        this.openCurrentPlayList = value;
     }
     
 }
