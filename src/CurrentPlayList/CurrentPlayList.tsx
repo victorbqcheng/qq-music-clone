2f6eb6250889549e3eeb08d5ecdfe34fc669f300
victor <victoratnz@outlook.com>
2025-08-01 00:10:44 +0800

    feat: implement click outside to close CurrentPlayList and refactor component structure

--- src/CurrentPlayList/CurrentPlayList.tsx
+++ src/CurrentPlayList/CurrentPlayList.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { PiListChecks } from "react-icons/pi";
+import stateStore from '../store/stateStore';
+import { observer } from 'mobx-react-lite';
 
-const CurrentPlayList = () => {
+const CurrentPlayList = observer(() => {
     const noOfTracks = 20; // 假设有10首曲目
+    const divRef = useRef<HTMLDivElement>(null);
+    useEffect(()=>{
+        const handleClickOutside = (event: MouseEvent) => {
+            
+            if (divRef.current && !divRef.current.contains(event.target as Node)) {
+                stateStore.setOpenCurrentPlayList(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
     return (
-        <div className=' absolute top-3 right-0 w-115 h-[calc(100%-100px)] bg-white select-none'>
-            <div className='flex flex-row items-center justify-between'>
+        <div className={`absolute top-3 ${stateStore.openCurrentPlayList ? 'right-0' : '-right-120'} w-115 h-[calc(100%-110px)] flex flex-col bg-gray-200 border-1 border-black select-none z-50 my-no-drag`}
+             ref={divRef}
+             tabIndex={1} autoFocus
+             onBlur={() => stateStore.setOpenCurrentPlayList(false)}>
+            <div className='flex flex-row items-center justify-between h-16'>
                 <div>播放队列</div>
                 <div className='flex flex-row gap-4'>
                     <PiListChecks className='text-2xl' />
                     <RiDeleteBin6Line className='text-2xl' />
                 </div>
             </div>
-            <div className='text-xs font-light'>共{noOfTracks}首歌曲</div>
-            <div className='overflow-y-auto custom-scrollbar h-[calc(100%-50px)]'>
+            <div className='text-xs font-light h-10'>共{noOfTracks}首歌曲</div>
+
+            <div className='overflow-y-auto custom-scrollbar'>
                 {Array.from({ length: noOfTracks }, (_, index) => (
                     <div key={index} className='flex flex-row items-center justify-between p-2 hover:bg-gray-100'>
                         <div>歌曲 {index + 1}</div>
@@ -22,8 +42,10 @@ const CurrentPlayList = () => {
                     </div>
                 ))}
             </div>
+
+
         </div>
     )
-}
+});
 
 export default CurrentPlayList
