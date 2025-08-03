da44fba588f85ff48688c4459651c2f8e52e39ee
victor <victoratnz@outlook.com>
2025-08-03 10:09:22 +0800

    feat: enhance playlist management with new item creation and renaming functionality

--- src/Page1/Left/CustomPlayLists.tsx
+++ src/Page1/Left/CustomPlayLists.tsx
@@ -1,22 +1,97 @@
-import React from 'react'
-import { PlayListItem } from './PredefinedPlayLists'
+import React, { useEffect } from 'react'
 import { BsFileMusic } from "react-icons/bs";
 import { MdOutlineCollections } from "react-icons/md";
+import customPlayListsStore from '../../store/customPlayListsStore';
+import PlayListItem from '../../components/PlayListItem';
+import { observer } from 'mobx-react-lite';
 
 
-const CustomPlayLists = () => {
+const CustomPlayLists = observer(() => {
+    const [isCreateintNewItem, setIsCreatingNewItem] = React.useState(false);
+    const handleCreateNewItem = () => {
+        setIsCreatingNewItem(true);
+    };
+    const handleBlur = (name: string) => {
+        setIsCreatingNewItem(false);
+        if (name.trim() === '') {
+            alert('歌单名称不能为空');
+            const defaultName = customPlayListsStore.getNextDefaultPlayListName();
+            customPlayListsStore.addPlayList(defaultName, []);
+        } else {
+            // 检查是否已存在同名歌单
+            const existingPlayList = customPlayListsStore.getAllPlayLists().get(name);
+            if (existingPlayList) {
+                alert(`歌单 "${name}" 已存在，建议修改歌单名称`);
+                // 如果存在同名歌单，则使用默认名称
+                name = customPlayListsStore.getNextDefaultPlayListName();
+            }
+            customPlayListsStore.addPlayList(name, []);
+        }
+
+    }
+
     return (
         <div className='flex flex-col gap-2'>
             <div className='flex flex-row gap-2 justify-start items-center'>
                 <div className='text-[14px] font-light'>自建歌单</div> |
                 <div className='text-[14px] text-gray-400 font-light'>收藏歌单</div>
-                <div className=' ml-auto text-gray-500 font-light hover:text-gray-900 p-2 cursor-pointer'>+</div>
+                <div className=' ml-auto text-gray-500 font-light hover:text-gray-900 p-2 cursor-pointer'
+                    onClick={handleCreateNewItem}>+</div>
             </div>
+            {isCreateintNewItem && <CreateNewItem onBlur={handleBlur} name={customPlayListsStore.getNextDefaultPlayListName()} />}
+            {
+                // 倒序添加
+                Array.from(customPlayListsStore.getAllPlayLists().keys()).reverse().map((name) => (
+                    <PlayListItem key={name} link={`/custome-playlist/${name}`} name={name} icon={<BsFileMusic className='w-6 h-6' />} />
+                ))
+            }
+            <PlayListItem link='/custome-playlist/1' name='默认列表' icon={<BsFileMusic className='w-6 h-6' />} />
+            <PlayListItem link='/custome-playlist/2' name='默认收藏' icon={<MdOutlineCollections className='w-6 h-6' />} />
 
-            <PlayListItem link='/custome-playlist/1' name='默认列表' icon={<BsFileMusic className='w-6 h-6'/>}/>
-            <PlayListItem link='/custome-playlist/2' name='默认收藏' icon={<MdOutlineCollections className='w-6 h-6'/>} />
         </div>
     )
-}
+})
+
+type CreateNewItemProps = {
+    name?: string;
+    onBlur?: (name: string) => void;
+};
+
+const CreateNewItem = ({ name, onBlur }: CreateNewItemProps) => {
+    const [inputValue, setInputValue] = React.useState(name);
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, []);
+    const handleInputFocus = () => {
+        if (inputRef.current) {
+            inputRef.current.select();
+        }
+    };
+
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    return (
+        <>
+            <div className={`flex flex-row items-center gap-2 p-2 hover:bg-gray-100 rounded-lg cursor-pointer`}>
+                {null}
+                <input className='text-sm text-gray-700'
+                    ref={inputRef}
+                    value={inputValue}
+                    type='text'
+                    aria-label=''
+                    placeholder='请输入歌单名称'
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    onBlur={() => onBlur(inputValue)} />
+            </div>
+        </>
+    );
+};
 
 export default CustomPlayLists
--- src/store/customPlayListsStore.tsx
+++ src/store/customPlayListsStore.tsx
@@ -29,6 +29,18 @@ class CustomPlayListsStore {
     getAllPlayLists(): Map<string, AudioFileInfo[]> {
         return this.customPlayLists;
     }
+    renamePlayList(oldName: string, newName: string) {
+        if (this.customPlayLists.has(newName)) {
+            alert(`歌单 "${newName}" 已存在，建议修改歌单名称`);
+            return;
+        }
+        const files = this.customPlayLists.get(oldName);
+        if (files) {
+            this.customPlayLists.delete(oldName);
+            this.customPlayLists.set(newName, files);
+            localStorage.setItem('customPlayLists', JSON.stringify(this.customPlayLists));
+        }
+    }
     // 找到下一个默认的歌单名称
     getNextDefaultPlayListName(): string {
         let index = 1;
