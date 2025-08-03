211312ef9f5c5f0846f5593297e0101342d987d4
victor <victoratnz@outlook.com>
2025-08-03 10:06:37 +0800

    feat: add @ant-design/v5-patch-for-react-19 dependency for enhanced compatibility

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -38,6 +38,7 @@ import LocalAndDownload from './Page1/Right/Main/LocalAndDownload/LocalAndDownlo
 import Trial from './Page1/Right/Main/Trial/Trial';
 import CustomPlayListPage from './Page1/Right/Main/CustomePlayList/CustomPlayListPage';
 import { PlayerContextProvider } from './context/PlayerContext';
+import '@ant-design/v5-patch-for-react-19';
 
 
 // const root = createRoot(document.body);
